8941f3b13d8f66206146e41a2bcf379c2def08c3
yahya omar ali ahmad <mugewara@hotmail.com>
2018-03-04 23:13:19 +0200

    Added a website

---
+++ WebSite1/Home.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Home.aspx.cs" Inherits="Home" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="main.css" rel="stylesheet" type="text/css" />
+    <title></title>
+</head>
+<body>
+
+    <div class='header ele'> <!-- This is the header --> 
+    This is a header
+    </div>
+
+    <div runat="server" id='target' class='contain ele'> <!-- This is the content --> 
+
+
+
+    </div>
+
+</body>
+</html>
---
+++ WebSite1/Home.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EasyConnection;
+using System.Data.SqlClient;
+
+public partial class Home : System.Web.UI.Page
+{
+    string cs = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\yhy0152612\\Desktop\\WebSite1\\App_Data\\MyDatabase.mdf;Integrated Security=True;User Instance=True";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        target.InnerHtml += "<table><tr><th>ID</th><th>Name</th><th>Address</th></tr>";
+        SqlConnection con = new SqlConnection(cs);
+        for (int i = 1; i <= Sql.GetRowCount(con, "Usernames"); i++) {
+            
+            string[] row = Sql.GetRow(con, String.Format("Select * from Usernames where id={0};",i));
+            target.InnerHtml += String.Format("<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>", row[0], row[1], row[2]);
+
+        }
+        target.InnerHtml+="</table>";
+    }
+}
